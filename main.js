33295357918fd975c3374086a6591feff060df14
Maximilian <74429157+mvf-rata@users.noreply.github.com>
2020-11-25 16:27:08 +0100

    Add files via upload
    Not much CSS, more Javascript

---
+++ main.js
@@ -0,0 +1,23 @@
+const answer = document.getElementById('answer');
+const button = document.getElementById('start');
+button.addEventListener('click', isPrime);
+
+function isPrime() {
+  let n = document.getElementById('input').value; // get the value when function runs.
+  if (n === 1) {
+    answer.innerHTML = '1 is neither a prime nor a composit number. It is a unit.';
+  } else if (n < 0) {
+    answer.innerHTML = 'It must be a positiv number.'
+    answer.style.color = 'red';
+  }
+  for (const d = 2; d <= n; d++){ //modulus operation
+    if (n % d == 0){
+      answer.innerHTML = 'It is not a Prime Number';
+      answer.style.color = 'red';
+      break;
+    } else {
+      answer.innerHTML = 'It is a Prime Number';
+      answer.style.color = 'green';
+    }
+  }
+}
